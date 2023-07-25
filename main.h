42bc78ce91cebf0f0d049d77e9dc9857ecb063fc
shaunpetro <shaun.p.mckoy@gmail.com>
2023-07-25 00:29:00 +0000

    update 3

--- main.h
+++ main.h
@@ -32,18 +32,11 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
- * @fm_t: The function associated.
- */
-
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-		va_list list, char buffer[], int flags, int width, int precision, int size);
+		                 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /* ------ FUNCTIONS ------ */
 
